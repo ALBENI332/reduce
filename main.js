9d69961b70c7199821ab17eba2a586b997d4d2f5
Benaya Aidarov <128293964+ALBENI332@users.noreply.github.com>
2024-10-30 19:35:29 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,187 @@
+// const nums = [1, 2, 3, 4, 5] 
+ 
+// const sum = nums.reduce((prev, item) => { 
+//     return prev + item  
+// }) 
+// console.log(sum) 
+ 
+ 
+ 
+ 
+ 
+//_________________________________ 
+ 
+ 
+ 
+ 
+ 
+// const nums = [1, 2, 3, 4, 5] 
+ 
+// const sum = nums.reduce((prev, item) => { 
+//     return prev * item  
+// }) 
+// console.log(sum) 
+ 
+ 
+ 
+ 
+//_______________________________ 
+ 
+ 
+ 
+// const nums = [10, 5, 8, 20, 3] 
+ 
+// const max = nums.reduce((prev, item) => { 
+//     return 
+// }) 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+// const num = [2, 3, 5, 4, 4, 1, 2] 
+ 
+// let temp = 0 
+ 
+// for (let j = num.length-1; j>=0; j--) { 
+//     for(let i=0; i<j; i++){ 
+//         if(num[i] > num[i+1]){ 
+//             let temp = num[i + 1] 
+//             num[i] = num[i+1] 
+//             num[i+1] = temp 
+//         } 
+//     } 
+// }  
+ 
+ 
+ 
+ 
+// const num = [2, 3, 4, 5, 7, 976] 
+ 
+// num.push(2323) 
+ 
+// console.log(num) 
+ 
+// num.pop(num) 
+ 
+// console.log(num) 
+ 
+// num.shift() 
+// num.unshift(34) 
+ 
+// console.log(num) 
+ 
+ 
+//______________________________________________ 
+ 
+ 
+ 
+ 
+// const num = [1, 2, 3, 4, 5, 6]; 
+ 
+// const even = [] 
+ 
+// num.forEach((item) => { 
+//     if(item % 2 === 0 ){ 
+//         even.push(item) 
+//     } 
+// }) 
+// console.log(even) 
+ 
+ 
+//_______________________________________________ 
+ 
+ 
+ 
+ 
+// const strings = ["apple", "banana", "cherry", "date"]; 
+ 
+// const done = [] 
+ 
+// strings.forEach((item) => { 
+//     if(item.length > 5){ 
+//         done.push(item) 
+//     } 
+// }) 
+// console.log(done) 
+ 
+ 
+ 
+ 
+//_________________________________________ 
+ 
+ 
+ 
+ 
+ 
+// const numbers = [-5, 3, -2, 8, 0, -1, 7]; 
+ 
+// const done = [] 
+ 
+// const done1 = [] 
+ 
+// numbers.forEach((item) => { 
+//     if (item >= 0) { 
+//         done.push(item) 
+//     } 
+//     else { 
+//         done1.push(item) 
+//     } 
+// }) 
+ 
+// console.log(done) 
+ 
+// console.log(done1) 
+ 
+ 
+ 
+ 
+ 
+//____________________________________ 
+ 
+ 
+ 
+ 
+ 
+// const numbers = [1, 2, 3, 2, 4, 1, 5]; 
+ 
+// const done = [] 
+ 
+// numbers.forEach((item) => { 
+//     if(!done.includes(item)){ 
+//         done.push(item) 
+//     } 
+// }) 
+ 
+// console.log(done) 
+ 
+ 
+//_________________________ 
+ 
+ 
+ 
+ 
+// const strings = ["hello", "world1", "test", "abc123", "example"] 
+ 
+// const done = [] 
+ 
+// const done1 = [] 
+ 
+// strings.forEach((item) => { 
+//     if(!done.includes(Number)){ 
+//         done.push(item) 
+//     } 
+//     else{ 
+//         done1.push(item) 
+//     } 
+// }) 
+ 
+// console.log(done) 
+// console.log(done1)
